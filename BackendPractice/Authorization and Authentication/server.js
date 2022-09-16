9f26e3393f05720047f01a5ffbf55909267835d4
pneumatick <jhwwefan@gmail.com>
2022-09-16 16:37:42 -0400

    Registration added, bcrypt password encryption added

--- BackendPractice/Authorization and Authentication/server.js
+++ BackendPractice/Authorization and Authentication/server.js
@@ -3,9 +3,11 @@ const app = express();
 const session = require('express-session');
 const store = new session.MemoryStore();
 const morgan = require('morgan');
+const bcrypt = require('bcrypt');
 
 const PORT = process.env.PORT || 8080;
-const users = {username: 'test', password: 'pass'};
+//const users = {username: 'test', password: 'pass'};
+const users = {};
 
 app.use(morgan('dev'));
 //app.use(express.json());
@@ -22,6 +24,7 @@ app.use(
 //app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
+// Authentication middleware
 function verifyAuthentication(req, res, next) {
     if (req.session.authenticated) {
         return next();
@@ -35,21 +38,57 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
 app.get('/landingpage', verifyAuthentication, (req, res) => {
     res.render('landingpage');
 });
 
-app.post('/login', (req, res) => {
+app.post('/register', async (req, res) => {
     const { username, password } = req.body;
-    if (users.username !== username) { return res.status(403).json({ msg: `User ${username} not found`}) };
+    try {
+        if (users[username]) { return res.status(403).json({ msg: `User ${username} already exists!` }) }
+        
+        const salt = await bcrypt.genSalt(10);
+        const hashedPass = await bcrypt.hash(password, salt);
+        users[username] = { password: hashedPass };
+        console.log(`New user ${username} created!`);
+
+        res.redirect('/');
+    }
+    catch (err) {
+        res.status(500).json({ msg: err.message });
+    }
+});
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    /*if (users.username !== username) { return res.status(403).json({ msg: `User ${username} not found` }) }
     if (users.password === password) {
         req.session.authenticated = true;
         req.session.user = { username: username, password: password };
         console.log(req.session);
         res.redirect('/landingpage');
+    }*/
+    try {
+        if (!users[username]) { return res.status(403).json({ msg: `User ${username} not found` }) }
+
+        const matchedPass = await bcrypt.compare(password, users[username].password);
+        //if (users[username].password === password) {
+        if (matchedPass) {
+            req.session.authenticated = true;
+            req.session.user = { username: username, password: password };
+            console.log(req.session);
+            res.redirect('/landingpage');
+        }
+        else {
+            res.status(403).json({ msg: "Bad Credentials" });
+        }
     }
-    else {
-        res.status(403).json({ msg: "Bad Credentials" });
+    catch (err) {
+        res.status(500).json({ msg: err.msg });
     }
 });
 
