dba9a78eb51427dd32198c4b5f8d2d90133974a7
pneumatick <jhwwefan@gmail.com>
2022-09-19 16:29:47 -0400

    SQLite database for user registration has been implemented

--- BackendPractice/Authorization and Authentication/server.js
+++ BackendPractice/Authorization and Authentication/server.js
@@ -4,6 +4,8 @@ const session = require('express-session');
 const store = new session.MemoryStore();
 const morgan = require('morgan');
 const bcrypt = require('bcrypt');
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('./Databases/users.sqlite');
 
 const PORT = process.env.PORT || 8080;
 const users = {};
@@ -46,11 +48,13 @@ app.get('/landingpage', verifyAuthentication, (req, res) => {
 app.post('/register', async (req, res) => {
     const { username, password } = req.body;
     try {
-        if (users[username]) { return res.status(403).json({ msg: `User ${username} already exists!` }) }
+        const userExists = await queryUser(username);
+        if (userExists) { return res.status(403).json({ msg: `User ${username} already exists!` }) }
         
         const salt = await bcrypt.genSalt(10);
         const hashedPass = await bcrypt.hash(password, salt);
-        users[username] = { password: hashedPass };
+        const newUser = await registerUser(username, hashedPass);
+
         console.log(`New user ${username} created!`);
 
         res.redirect('/');
@@ -62,14 +66,16 @@ app.post('/register', async (req, res) => {
 
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
-    try {
-        if (!users[username]) { return res.status(403).json({ msg: `User ${username} not found` }) }
 
-        const matchedPass = await bcrypt.compare(password, users[username].password);
+    try {
+        const user = await queryUser(username);
+        if (!user) { return res.status(403).json({ msg: `User ${username} not found` }) }
+
+        const matchedPass = await bcrypt.compare(password, user.password);
         if (matchedPass) {
             req.session.authenticated = true;
             req.session.user = { username: username, password: password };
-            console.log(req.session);
+            console.log(req.session); // just for testing, obviously insecure
             res.redirect('/landingpage');
         }
         else {
@@ -80,6 +86,37 @@ app.post('/login', async (req, res) => {
         res.status(500).json({ msg: err.msg });
     }
 });
+
+// Query the database for a given username, return its row
+function queryUser(username) {
+    return new Promise((resolve, reject) => {
+        db.get('SELECT * FROM Users WHERE username=$username', {
+            $username: username
+        }, (error, row) => {
+            if (error) {
+                console.log(error);
+                reject(error);
+            }
+            resolve(row);
+        });
+    });
+}
+
+// Add a new user to the database if the userame is not already taken
+function registerUser(username, password) {
+    return new Promise((resolve, reject) => {
+        db.run("INSERT INTO Users (username, password) VALUES ($username, $password)", {
+            $username: username,
+            $password: password,
+        }, (error) => {
+            if (error) {
+                console.log(error);
+                reject(error);
+            }
+            resolve();
+        });
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
